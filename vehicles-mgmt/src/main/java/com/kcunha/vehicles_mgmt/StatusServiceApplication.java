75f6f62306c1f70456331cbd7b4996d10237be29
kaiohenricunha <kaiohsdc@gmail.com>
2024-09-28 18:48:44 -0300

    Initial commit with Eureka Server and Status Service setup

---
+++ vehicles-mgmt/src/main/java/com/kcunha/vehicles_mgmt/StatusServiceApplication.java
@@ -0,0 +1,11 @@
+package com.kcunha.vehicles_mgmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StatusServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(StatusServiceApplication.class, args);
+    }
+}
---
+++ vehicles-mgmt/src/test/java/com/kcunha/vehicles_mgmt/VehiclesMgmtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kcunha.vehicles_mgmt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehiclesMgmtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
